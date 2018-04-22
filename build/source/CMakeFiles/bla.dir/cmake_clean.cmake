2549d692122c98558a17b7fe3547bc45559f74d7
Clara Metz <metz-c@macbook-pro-cm.localdomain>
2018-04-23 01:40:14 +0200

    Stand Aufgabe 1.11

---
+++ build/source/CMakeFiles/bla.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bla.dir/bla.cpp.o"
+  "bla.pdb"
+  "bla"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bla.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
